8f39acc87a1cfbd6f90a8b19974ecdbea7a816f8
PhamCong01 <phamvancong300701@gmail.com>
2022-10-08 16:33:15 +0700

    fix xiu xiu

--- lib/ui/course-report/course-report.dart
+++ lib/ui/course-report/course-report.dart
@@ -2,6 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:fruity/data/network/rest_client.dart';
+import 'package:fruity/data/sharedpref/constants/preferences.dart';
+import 'package:fruity/models/exam/exam.dart';
+import 'package:fruity/utils/notify_util.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CourseReportScreen extends StatefulWidget {
   const CourseReportScreen({Key? key}) : super(key: key);
@@ -12,9 +17,13 @@ class CourseReportScreen extends StatefulWidget {
 
 class _CourseReportScreen extends State<CourseReportScreen> {
   int _selectedIndex = 3;
+  List<Exam>? _exam;
+  bool isInProgress = false;
+
   @override
   void initState() {
     super.initState();
+    _getExam();
   }
 
   @override
@@ -30,6 +39,37 @@ class _CourseReportScreen extends State<CourseReportScreen> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+    });
+  }
+
+  _getExam() async {
+    setState(() {
+      isInProgress = true;
+    });
+    SharedPreferences _preferences = await SharedPreferences.getInstance();
+    var token = await _preferences.getString(Preferences.token);
+    await RestClient().get("/exam", headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer $token"
+    }).then((value) async {
+      List<dynamic>? body = jsonDecode(value.body) as List<dynamic>;
+      setState(() => {
+            _exam = body
+                .map<Exam>(
+                    (json) => Exam.fromJson(json as Map<String, dynamic>))
+                .toList()
+          });
+    }).catchError((error) {
+      print(error);
+      NotifyHelper.error(context, "Something went wrong");
+    }).whenComplete(() {
+      print("oke");
+      print(_exam);
+      if (mounted) {
+        setState(() {
+          isInProgress = false;
+        });
+      }
     });
   }
 
@@ -319,75 +359,15 @@ class _CourseReportScreen extends State<CourseReportScreen> {
       Container(
           child: Column(
         children: [
-          Container(
-              child: Column(children: [
-            Container(
-                margin: EdgeInsets.only(bottom: 15),
-                padding:
-                    EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 20),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
-                  boxShadow: [
-                    BoxShadow(
-                      color:
-                          Color.fromARGB(255, 217, 216, 216).withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Friday, 09 August 2019",
-                          style: TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w600,
-                          )),
-                      Container(
-                          margin: EdgeInsets.only(top: 10),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                CircleAvatar(
-                                  radius: 30,
-                                  backgroundImage: NetworkImage(
-                                      "https://scontent.fsgn2-1.fna.fbcdn.net/v/t39.30808-6/308505217_1594721400924906_3025204205154832825_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2lJNGFkqN3MAX95hzcy&tn=MMQY6WMhs1Yzm49w&_nc_ht=scontent.fsgn2-1.fna&oh=00_AT-7ehVgitOfs9wIgkFlO58xGwK9IcqyoInjUVz_jeQ75g&oe=633D3EFE"),
-                                ),
-                                Container(
-                                    child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Education consultantion",
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w500)),
-                                    const SizedBox(height: 5),
-                                    Text("Consultantion guidance",
-                                        style: TextStyle(
-                                            fontSize: 14,
-                                            color: Color.fromARGB(
-                                                255, 142, 141, 141))),
-                                  ],
-                                )),
-                                Text("8:30",
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        color:
-                                            Color.fromARGB(255, 188, 188, 188)))
-                              ]))
-                    ]))
-          ])),
-          Container(
-              margin: EdgeInsets.only(bottom: 15),
-              child: Column(children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _exam!.length,
+            itemBuilder: (_, index) {
+              Exam examData = _exam![index];
+              print("examdata");
+              print(examData);
+              return Container(
+                  child: Column(children: [
                 Container(
                     padding: EdgeInsets.only(
                         top: 10, left: 10, right: 10, bottom: 20),
@@ -401,8 +381,7 @@ class _CourseReportScreen extends State<CourseReportScreen> {
                           bottomRight: Radius.circular(10)),
                       boxShadow: [
                         BoxShadow(
-                          color: Color.fromARGB(255, 194, 193, 193)
-                              .withOpacity(0.5),
+                          color: Colors.grey.withOpacity(0.5),
                           spreadRadius: 5,
                           blurRadius: 7,
                           offset: Offset(0, 3), // changes position of shadow
@@ -433,90 +412,28 @@ class _CourseReportScreen extends State<CourseReportScreen> {
                                       crossAxisAlignment:
                                           CrossAxisAlignment.start,
                                       children: [
-                                        Text("Education consultantion",
+                                        Text(examData.name,
                                             style: TextStyle(
                                                 fontSize: 18,
                                                 fontWeight: FontWeight.w500)),
                                         const SizedBox(height: 5),
-                                        Text("Consultantion guidance",
+                                        Text(examData.subjectClass.name,
                                             style: TextStyle(
                                                 fontSize: 14,
                                                 color: Color.fromARGB(
                                                     255, 142, 141, 141))),
                                       ],
                                     )),
-                                    Text("8:30",
+                                    Text(examData.duration.toString(),
                                         style: TextStyle(
                                             fontSize: 14,
                                             color: Color.fromARGB(
                                                 255, 188, 188, 188)))
                                   ]))
                         ]))
-              ])),
-          Container(
-              child: Column(children: [
-            Container(
-                padding:
-                    EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 20),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Friday, 09 August 2019",
-                          style: TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w600,
-                          )),
-                      Container(
-                          margin: EdgeInsets.only(top: 10),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                CircleAvatar(
-                                  radius: 30,
-                                  backgroundImage: NetworkImage(
-                                      "https://scontent.fsgn2-1.fna.fbcdn.net/v/t39.30808-6/308505217_1594721400924906_3025204205154832825_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2lJNGFkqN3MAX95hzcy&tn=MMQY6WMhs1Yzm49w&_nc_ht=scontent.fsgn2-1.fna&oh=00_AT-7ehVgitOfs9wIgkFlO58xGwK9IcqyoInjUVz_jeQ75g&oe=633D3EFE"),
-                                ),
-                                Container(
-                                    child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Education consultantion",
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w500)),
-                                    const SizedBox(height: 5),
-                                    Text("Consultantion guidance",
-                                        style: TextStyle(
-                                            fontSize: 14,
-                                            color: Color.fromARGB(
-                                                255, 142, 141, 141))),
-                                  ],
-                                )),
-                                Text("8:30",
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        color:
-                                            Color.fromARGB(255, 188, 188, 188)))
-                              ]))
-                    ]))
-          ]))
+              ]));
+            },
+          )
         ],
       ))
     ])));
